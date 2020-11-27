825114498a5ae6c12a9365485719ab47300184b5
Luiz Guilherme <luizguilherme.lgsc@gmail.com>
2020-11-27 17:52:16 -0300

    Inicializando a api e buscando as iagens da web API

--- alurapic/src/app/app.component.ts
+++ alurapic/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'alurapic';
+
+	photos: Object[] = [];
+
+	constructor(http: HttpClient) {
+
+		http.get<Object[]>('http://localhost:3000/flavio/photos')
+			.subscribe(photos => this.photos = photos);
+
+	}
+
 }
